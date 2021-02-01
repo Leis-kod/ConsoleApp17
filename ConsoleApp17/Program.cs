92e064aee3756046a79d012b55ff4d0d05cbf59a
Alina-kod <lunaluna222428gmail.ru>
2021-02-01 17:06:34 +0600

    Добавьте файлы проекта.

---
+++ ConsoleApp17/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            ShowNumber();
+        }
+
+        static void ShowNumber()
+        {
+            Random random = new Random();
+            Console.WriteLine(random.Next());
+        }
+    }
+}
